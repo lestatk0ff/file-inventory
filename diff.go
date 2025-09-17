3fc03797b1e606404e41dbdd15a957b777ac145c
Anton Kaminski <anton_kaminski@epam.com>
2025-09-17 12:29:57 +0400

    Changed tests, changes output of diff

--- diff.go
+++ diff.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/tw"
 )
 
-// showDiff compares two files and prints lines unique to each
+// showDiff compares two files and prints lines unique to each in table format
 func showDiff(file1, file2 string) {
 	set1 := make(map[string]struct{})
 	set2 := make(map[string]struct{})
@@ -33,16 +36,52 @@ func showDiff(file1, file2 string) {
 		set2[scanner2.Text()] = struct{}{}
 	}
 
-	fmt.Println("Lines in", file1, "but not in", file2, ":")
+	// Collect all unique files
+	allFiles := make(map[string]struct{})
 	for line := range set1 {
 		if _, ok := set2[line]; !ok {
-			fmt.Println("-", line)
+			allFiles[line] = struct{}{}
 		}
 	}
-	fmt.Println("Lines in", file2, "but not in", file1, ":")
 	for line := range set2 {
 		if _, ok := set1[line]; !ok {
-			fmt.Println("+", line)
+			allFiles[line] = struct{}{}
 		}
 	}
+
+	// Create table
+	table := tablewriter.NewWriter(os.Stdout)
+	table.Options(
+		tablewriter.WithHeaderAutoFormat(tw.Off),
+		tablewriter.WithRendition(tw.Rendition{
+			Borders: tw.Border{
+				Left:   tw.Off,
+				Right:  tw.Off,
+				Top:    tw.Off,
+				Bottom: tw.Off,
+			},
+		}),
+	)
+	table.Header("file_path", file1, file2)
+
+	// Add differences to table
+	for filePath := range allFiles {
+		_, inFile1 := set1[filePath]
+		_, inFile2 := set2[filePath]
+
+		file1Status := " "
+		file2Status := " "
+
+		if inFile1 && !inFile2 {
+			file1Status = "+"
+			file2Status = "-"
+		} else if !inFile1 && inFile2 {
+			file1Status = "-"
+			file2Status = "+"
+		}
+
+		table.Append(filePath, file1Status, file2Status)
+	}
+
+	table.Render()
 }
